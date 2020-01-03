66e2387f8fb3d2d7f5ccc6a880cf3c2df791c92a
Asma1717 <f.kamiab1717@yahoo.com>
2020-01-03 15:34:00 -0500

    add a Captson project as an Assignment

---
+++ Captson_Project.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Capston project
+
+# ### This is a firs project
+
+# In[ ]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
